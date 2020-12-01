6e1785ae4644c7154d57cf3fb447c919128c1711
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-01 13:37:03 +0100

    Puzzle 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+const INPUT: &str = include_str!("input");
+
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<usize> = INPUT.split('\n').map(|x| x.parse().expect("not a number")).collect();
+
+    for x in &input {
+        for y in &input {
+            if x + y == 2020 {
+                println!("{} + {} = 2020", x, y);
+                println!("{} x {} = {}", x, y, x * y);
+            }
+        }
+    }
+
+    println!("{:?}", input);
 }
