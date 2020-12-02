eeb8fc830a62b313df60b06b8dc661e87efd762e
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-02 23:13:05 +0100

    Day 2: Part one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use scan_fmt::scan_fmt;
+
+const INPUT: &str = include_str!("input");
+
+fn count_matches() -> usize {
+    INPUT.lines()
+        .filter(|l| {
+            let (min, max, char, pw) = scan_fmt!(l, "{}-{} {}: {}", usize, usize, char, String).unwrap();
+            (min..=max).contains(&pw.chars().filter(|c| c == &char).count())
+        })
+        .count()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", count_matches());
 }
