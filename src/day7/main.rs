cd7b60e233e8ec07c3bd54ba5d7fb32a07550cdd
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-08 10:40:28 +0100

    Optimise day 7

--- src/day7/main.rs
+++ src/day7/main.rs
@@ -1,9 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::borrow::Borrow;
 
 fn main() {
     const INPUT: &str = include_str!("input");
 
-    let vec = INPUT.lines().map(Bag::from).collect::<Vec<Bag>>();
+    let vec = INPUT.lines().map(Bag::from).collect::<HashSet<Bag>>();
 
     println!("Answer 1: {}", vec.iter().filter(|b| {
         b.contains_indirect("shiny gold bag", &vec)
@@ -14,6 +16,18 @@ fn main() {
 
 #[derive(Eq, PartialEq, Debug)]
 struct Bag(String, HashMap<String, usize>);
+
+impl Hash for Bag {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state)
+    }
+}
+
+impl Borrow<String> for Bag {
+    fn borrow(&self) -> &String {
+        &self.0
+    }
+}
 
 impl From<&str> for Bag {
     fn from(s: &str) -> Self {
@@ -39,9 +53,9 @@ trait FindBag {
     fn find_bag(&self, name: &str) -> Option<&Bag>;
 }
 
-impl FindBag for &Vec<Bag> {
+impl FindBag for &HashSet<Bag> {
     fn find_bag(&self, name: &str) -> Option<&Bag> {
-        self.iter().find(|b| b.0 == name)
+        self.get(&name.to_string())
     }
 }
 
@@ -50,13 +64,13 @@ impl Bag {
         self.1.contains_key(other)
     }
 
-    fn contains_indirect(&self, other: &str, bags: &Vec<Bag>) -> bool {
+    fn contains_indirect(&self, other: &str, bags: &HashSet<Bag>) -> bool {
         self.contains(other) || self.1.keys().any(|k| {
             bags.find_bag(k).map_or_else(|| false, |b| b.contains_indirect(other, bags))
         })
     }
 
-    fn count_bags(&self, bags: &Vec<Bag>) -> usize {
+    fn count_bags(&self, bags: &HashSet<Bag>) -> usize {
         1 /*myself*/ + self.1.iter().map(|(bag_name, v)| {
             let bag = bags.find_bag(bag_name);
             (match bag {
